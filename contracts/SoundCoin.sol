d45e4c955272ad7a855224df45f116b94d8e8776
Sounddao <jeremy@sounddao.com>
2026-02-17 16:40:45 -0500

    Create SoundCoin ERC20 token smart contract

--- contracts/SoundCoin.sol
+++ contracts/SoundCoin.sol
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SoundCoin is ERC20 {
+contract SoundCoin is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("SoundCoin", "SND") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
