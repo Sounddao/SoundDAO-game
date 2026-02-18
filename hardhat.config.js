819b0b53ebac865210a71f0e6d029870eda1ec00
Sounddao <jeremy@sounddao.com>
2026-02-17 20:46:21 -0500

    Add hardhat.config.js file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,45 @@
-require('@nomiclabs/hardhat-waffle');
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    hardhat: {},
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
-      accounts: [`0xYOUR_PRIVATE_KEY`], // replace with your wallet private key
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
-      accounts: [`0xYOUR_PRIVATE_KEY`], // replace with your wallet private key
-    },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
   },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
